642c4e470b9cc61fa95f7424f9106e5dfc566cdc
Caioba <caio.henrique.1204@gmail.com>
2023-10-04 22:04:01 -0300

    login page

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class LoginPage {
+  cpf: string = "";
+  senha: string = "";
+
+  constructor(private navCtrl: NavController) {}
+
+  login() {
+    // Aqui você pode adicionar a lógica de login com os dados de CPF e senha
+    console.log('CPF:', this.cpf);
+    console.log('Senha:', this.senha);
+    // Adicione a lógica de autenticação e redirecionamento após o login
+  }
+
+  navigateToRegister() {
+    this.navCtrl.navigateForward('/register'); // Substitua pela rota correta para a página de registro
+  }
+}
